c8061362dbbd556a3bd079bb4a6acafac1dfb63c
PearsonBasham <basham.pearson@gmail.com>
2018-04-03 07:32:19 -0700

    In the AppDelegate I configured the appearance of the Tab bar buttons and navigation bar items to be black, removed the translucence. Created MVC Folders and placed respective files.

---
+++ ECommerceApp/Controllers/FeedTableViewController.swift
@@ -0,0 +1,43 @@
+//
+//  FeedTableViewController.swift
+//  ECommerceApp
+//
+//  Created by PEARSON BASHAM on 4/3/18.
+//  Copyright © 2018 darqfox. All rights reserved.
+//
+
+import UIKit
+
+class FeedTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+    
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return 0
+    }
+
+    /*
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+
+        // Configure the cell...
+
+        return cell
+    }
+    */
+
+    
+
+}
